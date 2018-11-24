5a1e176bcac2236c91a09ca581cc458d66734723
patriciovergaratobar <patricio.vergara.tobar@gmail.com>
2018-11-24 14:12:25 -0300

    Proyecto base, WS de CRUD usuario y Login

--- api/persistence/linkdb.php
+++ api/persistence/linkdb.php
@@ -1,10 +1,5 @@
 <?php
 
-//$link = mysql_connect('localhost', 'mmoya_holter1', 'vergara16')
-  //  or die('No se pudo conectar: ' . mysql_error());
-//echo 'Connected successfully';
-//mysql_select_db('mmoya_holter1') or die('No se pudo seleccionar la base de datos');
-
 function getConnect() {
     // Conectando, seleccionando la base de datos
     $link = mysqli_connect('localhost:3306', 'root', 'root12345', 'estado_obras');
